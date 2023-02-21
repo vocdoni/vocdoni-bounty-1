e61c8a25b2591860500f104ff53b7973e4b182a8
Gerard <g.roura.v@protonmail.com>
2023-02-21 09:31:51 +0000

    protected routes

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
+import ProtectedRoutes from '../components/ProtectedRoutes';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home';
 
@@ -10,7 +11,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route element={null}>
+      <Route element={<ProtectedRoutes />}>
         <Route path="createlection" element={null} />
         <Route path="electionslist" element={null} />
       </Route>
