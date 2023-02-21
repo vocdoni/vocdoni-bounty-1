bdef615d0e22da041cff8af9c4f453fef5842cb4
Gerard <g.roura.v@protonmail.com>
2023-02-21 09:03:43 +0000

    rainbow kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme, useColorMode } from '@chakra-ui/react';
+import { darkTheme, lightTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { RouterProvider } from 'react-router-dom';
+import { WagmiConfig } from 'wagmi';
+import { chains, wagmiClient } from './lib/rainbow';
 import router from './router/Router';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
-    <RouterProvider router={router} />
-  </ChakraProvider>
-);
+export const App = () => {
+  const { colorMode } = useColorMode();
+
+  const rainbowStyles =
+    colorMode === 'light'
+      ? lightTheme({
+          accentColor: '#78D8AA',
+          accentColorForeground: 'white',
+          borderRadius: 'medium',
+        })
+      : darkTheme({
+          accentColor: '#78D8AA',
+          accentColorForeground: 'white',
+          borderRadius: 'medium',
+        });
+
+  return (
+    <ChakraProvider theme={theme}>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={rainbowStyles}>
+          {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+          <RouterProvider router={router} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
+};
